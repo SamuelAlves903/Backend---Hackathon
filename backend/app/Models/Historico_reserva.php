26cfac149d76d79b5473762d3284de351d3995a4
Samuel Alves <samuelalves205@gmail.com>
2024-11-19 21:01:01 -0300

    Estruturação Laravel

---
+++ backend/app/Models/Historico_reserva.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Historico_reserva extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['reserva_id', 'alteracoes', 'modificado_em'];
+}
---
+++ backend/app/Models/Reserva.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reserva extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['usuario_id', 'ambiente_id','horario_inicio', 'horario_fim', 'status'];
+}
